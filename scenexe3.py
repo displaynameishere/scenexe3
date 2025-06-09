8d22effed178d3e4024d0f3516a0149d6322e5dd
displaynameishere <displaynamecodes@gmail.com>
2025-06-09 16:04:28 -0700

    Ignore .pngs

--- scenexe3.py
+++ scenexe3.py
@@ -1,6 +1,14 @@
+# scenexe3 bot
+# made by displaynameis here
 import undetected_chromedriver as uc
+from time import sleep
 
-driver = uc.Chrome()
+HEADLESS = True
+PAGE_LOAD_WAIT = 5
+TEMP_SCREENSHOTS_DIR = "~/.scenexe3/temp_frames"
+
+driver = uc.Chrome(headless=HEADLESS)
 driver.get("https://scenexe2.io")
+sleep(PAGE_LOAD_WAIT)
 driver.save_screenshot("test.png")
 driver.quit()
